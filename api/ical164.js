3ef31547e01810822bfe3ddb0abc37b7d9b9f4e7
casac-hub <clara@casatonda.ch>
2026-05-06 16:55:39 +0200

    Create ical164.js

---
+++ api/ical164.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  try {
+    const r = await fetch('http://cleaning.cybhotel.net/room_calendar_a?hotel_ext_id=164');
+    const text = await r.text();
+    res.setHeader('Content-Type', 'text/calendar');
+    return res.status(200).send(text);
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
+}
