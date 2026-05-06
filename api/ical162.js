8efb6c24a20556d82c6859e3df953f6783943ccc
casac-hub <clara@casatonda.ch>
2026-05-06 17:33:07 +0200

    Update ical162.js

--- api/ical162.js
+++ api/ical162.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   if (req.method === 'OPTIONS') return res.status(200).end();
   try {
-    const r = await fetch('http://cleaning.cybhotel.net/room_calendar_a?hotel_ext_id=162');
+    const r = await fetch('https://cleaning.cybhotel.net/room_calendar_a?hotel_ext_id=162');
     const text = await r.text();
     res.setHeader('Content-Type', 'text/calendar');
     return res.status(200).send(text);
